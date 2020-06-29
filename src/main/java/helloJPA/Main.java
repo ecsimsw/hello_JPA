5a356ffabde10229465fdb8e4374a430e17e2eff
ecsimsw <ecsimsw@gmail.com>
2020-06-30 06:03:03 +0900

    연관관계 편의 메소드

--- src/main/java/helloJPA/Main.java
+++ src/main/java/helloJPA/Main.java
@@ -27,51 +27,40 @@ public class Main {
             teamA.setName("TeamA");
             em.persist(teamA);
 
-            Member member_A = new Member();
-            member_A.setName("memberA");
-            member_A.setTeam(teamA);
-            em.persist(member_A);
-
-            em.flush();
-            em.clear();
-
-            Member findMember = em.find(Member.class, member_A.getId());
-            Team findTeam= findMember.getTeam();
-            System.out.println(findTeam.getName());
-
-
             Team teamB = new Team();
             teamB.setName("TeamB");
             em.persist(teamB);
 
-            Member member_B = new Member();
-            member_B.setName("memberB");
-            member_B.setTeam(teamB);
-            em.persist(member_B);
+            Member memberA = new Member();
+            memberA.setName("memberA");
+            memberA.setTeam(teamA);
+           // teamA.getMembers().add(memberA);
 
-            em.flush(); // find ok
+//            em.flush();
+//            em.clear();
 
-            findMember = em.find(Member.class, member_B.getId());
-            findTeam= findMember.getTeam();
-            System.out.println(findTeam.getName());
+            for(Member m : teamA.getMembers()){
+                System.out.println(m.getName());
+            }
 
-            Member member_C = new Member();
-            member_C.setName("memberB");
-            member_C.setTeam(teamB);
-            em.persist(member_C);
+            for(Member m : teamB.getMembers()){
+                System.out.println(m.getName());
+            }
 
-            member_C.setTeam(teamA);
-            // changing value after persist 
+            Team findTeamA = em.find(Team.class, teamA.getId());
 
-            em.flush();
+            for(Member m : findTeamA.getMembers()){
+                System.out.println("teamA : "+m.getName());
+            }
 
-            findMember = em.find(Member.class, member_C.getId());
-            findTeam= findMember.getTeam();
-            System.out.println(findTeam.getName());
+            Team findTeamB = em.find(Team.class, teamB.getId());
+
+            for(Member m : findTeamB.getMembers()){
+                System.out.println("teamB : " +m);
+            }
 
             tx.commit();
         }catch (Exception e){
-            System.out.println(e);
             tx.rollback();
         }finally{
             em.close();
--- src/main/java/helloJPA/entitiy/Member.java
+++ src/main/java/helloJPA/entitiy/Member.java
@@ -8,7 +8,9 @@ import javax.persistence.*;
 @Entity
 @Getter @Setter
 public class Member {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+
     private Long id;
 
     @Column(name = "USERNAME")
@@ -17,4 +19,12 @@ public class Member {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
+
+    public void setTeam(Team team) {
+        if (this.team != null) {
+            this.team.getMembers().remove(this);
+        }
+        this.team = team;
+        team.getMembers().add(this);
+    }
 }
